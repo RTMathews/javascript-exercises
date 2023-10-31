0490b8d9a6be92c747e7f04ab1dcc435e29c5232
Ryan <mathewsthomasryan@gmail.com>
2023-10-31 07:05:13 -0500

    add forEach and if

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function(array, ...args) {
     const newArr = [];
+
+    array.forEach(item)
+    {
+        if (!args.includes(item))
+        {
+            newArr.push(item);
+        }
+    }
 };
 
 // Do not edit below this line
