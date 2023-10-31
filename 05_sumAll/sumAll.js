00eea11f00c7ab4a61fb8384d2b5cfe276778fd1
Ryan <mathewsthomasryan@gmail.com>
2023-10-31 10:17:37 -0500

    i += finalSum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -6,8 +6,9 @@ const sumAll = function(num1, num2) {
     }
     for (let i = num1; i < num2; i++)
     {
-        
+        i += finalSum;
     }
+    return finalSum;
 };
 
 // Do not edit below this line
