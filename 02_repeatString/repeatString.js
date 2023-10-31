2cc881a59f191adea0fae01602af64a4bdc956a0
Ryan <mathewsthomasryan@gmail.com>
2023-10-31 06:15:55 -0500

    add for loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
 const repeatString = function(string, num) {
     const str = string;
     const int = num;
+
+    for (let i = 0; i < int; i++)
+    {
+        
+    }
 };
 
 // Do not edit below this line
