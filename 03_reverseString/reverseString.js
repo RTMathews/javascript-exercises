25436efe56c5d1f3a25b4b469cece6a1c0c91540
Ryan <mathewsthomasryan@gmail.com>
2023-10-31 06:41:31 -0500

    remove all

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,5 @@
 const reverseString = function(word) {
-    let string = "";
-
-    for (let i = string.length; i >= 0; i--)
-    {
-        string += word;
-    }
-    return string;
+    
 };
 
 // Do not edit below this line
