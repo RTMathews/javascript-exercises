64eba8e0c8eee06e3e3b00bff59c2cf699d50c2b
Ryan <mathewsthomasryan@gmail.com>
2023-10-31 11:18:28 -0500

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,13 +4,13 @@ const sumAll = function(num1, num2) {
     {
         return 'ERROR';
     }
-    for (let i = num1; i < num2; i++)
+    for (let i = num1; i <= num2; i++)
     {
-        i += finalSum;
-        console.log(finalSum);
+       finalSum = i + finalSum;
     }
     return finalSum;
 };
+//console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
