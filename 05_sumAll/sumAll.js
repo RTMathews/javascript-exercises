3135da7095a345795a81833da7585af91cb8fbea
Ryan <mathewsthomasryan@gmail.com>
2023-10-31 11:22:18 -0500

    test 3

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,6 +4,10 @@ const sumAll = function(num1, num2) {
     {
         return 'ERROR';
     }
+    else if (typeof num1 === 'string')
+    {
+        return 'ERROR';
+    }
     for (let i = num1; i <= num2; i++)
     {
        finalSum = i + finalSum;
