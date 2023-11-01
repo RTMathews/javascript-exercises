d88acf4959e6b299a3012cb400b9b7537dd3b76a
Ryan <mathewsthomasryan@gmail.com>
2023-11-01 09:05:28 -0500

    return convertToFarenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,11 @@
 const convertToCelsius = function(temp) {
   let toC = (temp - 32) * 5 / 9;
-  return Math.round(toC * 10) /10;
+  return Math.round(toC * 10) / 10;
 };
 //console.log(convertToCelsius(100));
 const convertToFahrenheit = function(temp) {
   let toF = (temp * 1.8) + 32;
+  return Math.round(toF * 10) / 10;
 };
 
 // Do not edit below this line
