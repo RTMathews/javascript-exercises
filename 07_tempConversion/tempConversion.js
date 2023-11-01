2a06fc57f485b5e11ce0104f563ddaf150acf923
Ryan <mathewsthomasryan@gmail.com>
2023-11-01 09:01:59 -0500

    comment test log

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,7 +2,7 @@ const convertToCelsius = function(temp) {
   let toC = (temp - 32) * 5 / 9;
   return Math.round(toC * 10) /10;
 };
-console.log(convertToCelsius(100));
+//console.log(convertToCelsius(100));
 const convertToFahrenheit = function() {
 };
 
