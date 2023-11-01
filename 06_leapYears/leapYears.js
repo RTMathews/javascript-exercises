ad1e28d676249853d295a23503e610a2c50f6331
Ryan <mathewsthomasryan@gmail.com>
2023-11-01 07:49:03 -0500

    comment out console.log

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,7 @@
 const leapYears = function(year) {
     if (year % 4 === 0 && year % 400 === 0)
     {
-        if (year % 100 !== 0)
+        if (year % 100 === 0)
         {
             return true;
         }
@@ -15,7 +15,7 @@ const leapYears = function(year) {
         return false;
     }
 };
-console.log(leapYears(2000));
+//console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
