782e17b0f53382e439847d24993c21b46e15445f
Ryan <mathewsthomasryan@gmail.com>
2023-11-01 07:52:12 -0500

    remove nested if

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,21 +1,15 @@
 const leapYears = function(year) {
     if (year % 4 === 0 && year % 400 === 0)
     {
-        if (year % 100 === 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        
+    }
     }
     else
     {
         return false;
     }
 };
-//console.log(leapYears(2000));
+console.log(leapYears(1996));
 
 // Do not edit below this line
 module.exports = leapYears;
