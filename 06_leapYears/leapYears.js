d89ddf8606549eb20511b14c13f4df31413881e2
Ryan <mathewsthomasryan@gmail.com>
2023-11-01 07:55:30 -0500

    remove all

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,19 +1,5 @@
 const leapYears = function(year) {
-    if (year % 100 === 0 && year % 400 === 0)
-    {
-        if (year % 4 === 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    else
-    {
-        return false;
-    }
+    
 };
 console.log(leapYears(1996));
 
