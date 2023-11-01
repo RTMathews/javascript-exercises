ca34cb6a057d7ec7ab3998b386f360397f55fb7a
Ryan <mathewsthomasryan@gmail.com>
2023-11-01 09:06:31 -0500

    comment out test log

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -7,7 +7,7 @@ const convertToFahrenheit = function(temp) {
   let toF = (temp * 1.8) + 32;
   return Math.round(toF * 10) / 10;
 };
-console.log(convertToFahrenheit(0));
+//console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
