be78d52bb390257567288534ebef740c8a0943db
Ryan <mathewsthomasryan@gmail.com>
2023-11-01 07:54:57 -0500

    nested if

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,14 @@
 const leapYears = function(year) {
     if (year % 100 === 0 && year % 400 === 0)
     {
-        
+        if (year % 4 === 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     else
     {
